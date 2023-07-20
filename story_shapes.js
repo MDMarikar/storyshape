5175253fe3e92923956db7ad304b31c5da4685e2
MD Marikar <dilshad.marikar@gmail.com>
2023-07-20 20:12:35 +0100

    Update story_shapes.js

--- story_shapes.js
+++ story_shapes.js
@@ -6,17 +6,17 @@ let yScale = d3.scaleLinear().domain([-1, 1]).range([400, 0]);
 
 // Line generator
 let line = d3.line()
-    .x((d, i) => xScale(i / (d.length - 1)))
-    .y(d => yScale(d))
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y))
     .curve(d3.curveBasis);  // Makes the line curvy
 
 // Story shapes data
 let storyShapes = {
-    "man-in-hole": Array.from({length: 100}, (_, i) => (i < 50 ? -i / 50 : (i - 50) / 50)),
-    "boy-meets-girl": Array.from({length: 100}, (_, i) => (i < 25 ? i / 25 : (i < 75 ? 2 - i / 25 : i / 25 - 2))),
-    "kafkaesque": Array.from({length: 100}, (_, i) => -i / 100),
-    "hamlet": Array.from({length: 100}, (_, i) => Math.sin(i / 100 * 2 * Math.PI)),
-    "cinderella": Array.from({length: 100}, (_, i) => (i < 25 ? i / 25 : (i < 50 ? 1 - (i - 25) / 25 : (i < 75 ? (i - 50) / 25 : 1))))
+    "man-in-hole": Array.from({length: 100}, (_, i) => ({x: i / 99, y: i < 50 ? -i / 50 : (i - 50) / 50})),
+    "boy-meets-girl": Array.from({length: 100}, (_, i) => ({x: i / 99, y: i < 25 ? i / 25 : (i < 75 ? 2 - i / 25 : i / 25 - 2)})),
+    "kafkaesque": Array.from({length: 100}, (_, i) => ({x: i / 99, y: -i / 100})),
+    "hamlet": Array.from({length: 100}, (_, i) => ({x: i / 99, y: Math.sin(i / 100 * 2 * Math.PI)})),
+    "cinderella": Array.from({length: 100}, (_, i) => ({x: i / 99, y: i < 25 ? i / 25 : (i < 50 ? 1 - (i - 25) / 25 : (i < 75 ? (i - 50) / 25 : 1))}))
 };
 
 // Draw story shapes
